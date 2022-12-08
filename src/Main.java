534a7ee3ba86e5ba4cd3b4709180ff299aad1c54
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-12-08 14:21:14 -0600

    intro to java exercise

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import java.awt.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("We do what we must because we can!");
+        System.out.println("Apperture science");
+    }
+//public class Bobby {
+    //public static void bobby(String[] args) {
+//System.out.println("Todd & Arron");
+
+    //public class
+
+
+
+
 }
